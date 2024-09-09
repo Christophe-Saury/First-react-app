acec9f8be20b53240342b5849d20e6fccace4837
Christophe SAURY <christophesaury23@gmail.com>
2024-09-09 18:20:45 +0200

    replaced listgroup code by alert code in main funct

--- first-react-app/src/App.tsx
+++ first-react-app/src/App.tsx
@@ -1,19 +1,10 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["New york", "SF", "tokyo", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert />
     </div>
   );
 }

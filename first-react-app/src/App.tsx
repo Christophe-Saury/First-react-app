d1ceb4bb0d0b52f81017468c8f20438328971cae
Christophe SAURY <christophesaury23@gmail.com>
2024-09-09 19:24:24 +0200

    added children parameter to button

--- first-react-app/src/App.tsx
+++ first-react-app/src/App.tsx
@@ -1,12 +1,11 @@
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 
 function App() {
   return (
     <div>
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
+      <Button>My button</Button>
     </div>
   );
 }

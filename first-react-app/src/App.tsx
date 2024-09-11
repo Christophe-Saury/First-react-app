b81b687265be8472baeef31c2d73e58c3085fba1
Christophe SAURY <christophesaury23@gmail.com>
2024-09-11 17:01:09 +0200

    added an alert to the button plus a statechange

--- first-react-app/src/App.tsx
+++ first-react-app/src/App.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
-      <Button onClick={() => console.log("CLicked")}>My button</Button>
+      {alertVisible && <Alert> My Alert </Alert>}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>
+        My button
+      </Button>
     </div>
   );
 }
